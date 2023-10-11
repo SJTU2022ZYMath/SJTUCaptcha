0c1e0e4627a4115b15d950db06934526f93cdc5f
lytDARK <lytDARK@qq.com>
2023-10-12 01:43:43 +0800

    bat

--- pull.bat
+++ pull.bat
@@ -1,10 +1,12 @@
 ﻿::encoding: utf8
 @echo off
-@chcp 65001
-@color 0F
+chcp 65001
+color 0F
 :pull
 git pull
-echo.
-if %errorlevel% neq 0 goto pull
-echo ————拉取完成！————
+set error=%errorlevel%
+if %error% neq 0 echo 错误代码：%error%
+if %error% neq 0 echo.
+if %error% neq 0 goto pull
+echo ————拉取完成，请检查！————
 pause
--- sync.bat
+++ sync.bat
@@ -1,19 +1,22 @@
 ﻿::encoding: utf8
 @echo off
-@chcp 65001
-@color 0F
+chcp 65001
+color 0F
 echo ————请先使用pull.bat合并————
-@pause
+set /p input=是否拉取检查合并？(y：检查)
+if %input% == y call pull.bat
 :pull
 git pull
-set error = %errorlevel%
-echo %error%
+set error=%errorlevel%
+if %error% neq 0 echo 错误代码：%error%
+if %error% neq 0 echo.
 if %error% neq 0 goto pull
 echo ————拉取完成！合并中……————
 :push
 git push
-set error = %errorlevel%
-echo %error%
+set error=%errorlevel%
+if %error% neq 0 echo 错误代码：%error%
+if %error% neq 0 echo.
 if %error% neq 0 goto push
 echo ————拉取完成！按任意键退出————
-@pause
+pause
