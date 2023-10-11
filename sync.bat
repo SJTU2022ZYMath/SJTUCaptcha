d4ef9e2087855f9fdd5fecda032ac9a2437951ef
lytDARK <lytDARK@qq.com>
2023-10-12 01:15:05 +0800

    bat

--- sync.bat
+++ sync.bat
@@ -2,15 +2,18 @@
 @echo off
 @chcp 65001
 @color 0F
+echo ————请先使用pull.bat合并————
+@pause
 :pull
 git pull
-echo.
-if %errorlevel% neq 0 goto pull
+set error = %errorlevel%
+echo %error%
+if %error% neq 0 goto pull
+echo ————拉取完成！合并中……————
 :push
-echo ————拉取完成！请检查合并冲突后按任意键合并！————
-@pause
 git push
-echo.
-if %errorlevel% neq 0 goto push
+set error = %errorlevel%
+echo %error%
+if %error% neq 0 goto push
 echo ————拉取完成！按任意键退出————
 @pause
